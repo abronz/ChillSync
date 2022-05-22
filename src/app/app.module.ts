f31997be81495c92c8520889fa05c11e0b132b6a
Abronz Vizcarra <vabronz@gmail.com>
2022-05-21 21:30:44 -0400

    adding buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private sanitizer: DomSanitizer) { }
+
+  URL: string
+  safeURL: SafeResourceUrl
+  playerVars: any
 
   ngOnInit() {
+    this.URL = "https://www.youtube.com/watch?v=4VxdufqB9zg";
+    this.playerVars = "?disablekb=1&controls=0&modestbranding=1&rel=0&showinfo=0&autoplay=0"
+    this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/4VxdufqB9zg'+this.playerVars)
+
+
   }
 
 }
